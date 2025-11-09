67a64e94c3b422ffaa8a082f4aee93c9f3261ffb
Razeen Zakariya <razeenzak@gmail.com>
2025-11-09 17:36:23 +0000

    The button on qr registeration form is not working

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useTransition, useEffect, useActionState } from 'react';
+import React, { useState, useEffect, useActionState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
