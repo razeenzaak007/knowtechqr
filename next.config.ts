bd88f87920e15adf2ce7b34c0ec5b44153fa84f1
Razeen Zakariya <razeenzak@gmail.com>
2025-11-10 21:21:55 +0000

    <a href='https://postimg.cc/D8sgDrWM' target='_blank'><img src='https://

--- next.config.ts
+++ next.config.ts
@@ -40,6 +40,12 @@ const nextConfig: NextConfig = {
         hostname: 'i.imgur.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
           <Link href="/" className="flex items-center gap-3 text-xl font-bold transition-opacity hover:opacity-80">
             <div className="bg-muted p-1 rounded-md flex items-center justify-center">
                 <Image 
-                    src="https://i.imgur.com/8Nxt3hr.png"
+                    src="https://i.postimg.cc/RVpDWGv4/IMG-20251111-WA0000.jpg"
                     alt="Knowtech Logo"
                     width={90}
                     height={32}
